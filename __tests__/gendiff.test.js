ea14f0093257781202118c622ea39708572bfda0
kinelit <kinelit@mail.ru>
2017-05-05 23:04:59 +0400

    add AST

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,6 +9,9 @@ const afterYaml = './__tests__/__fixtures__/yaml/after.yml';
 const beforeIni = './__tests__/__fixtures__/ini/before.ini';
 const afterIni = './__tests__/__fixtures__/ini/after.ini';
 
+const nestedBeforeJson = './__tests__/__fixtures__/json/nested/before.json';
+const nestedAfterJson = './__tests__/__fixtures__/json/nested/after.json';
+
 const equal = `{
   host: hexlet.io
 + timeout: 20
@@ -16,6 +19,58 @@ const equal = `{
 - proxy: 123.234.53.22
 + verbose: true
 }`;
+
+const nestedEqual = `{
+  common: {
+  setting1: Value 1
+  - setting2: 200
+  setting3: true
+  - setting6: {
+  key: value
+  }
+  + setting4: blah blah
+  + setting5: {
+  key5: value5
+  }
+  }
+  group1: {
+  + baz: bars
+  - baz: bas
+  foo: bar
+  }
+  - group2: {
+  abc: 12345
+  }
+  + group3: {
+  fee: 100500
+  }
+}`;
+
+// const nestedEqual = `{
+//   common: {
+//      setting1: Value 1
+//       - setting2: 200
+//         setting3: true
+//       - setting6: {
+//             key: value
+//         }
+//       + setting4: blah blah
+//       + setting5: {
+//             key5: value5
+//         }
+//     }
+//     group1: {
+//       + baz: bars
+//       - baz: bas
+//         foo: bar
+//     }
+//   - group2: {
+//         abc: 12345
+//     }
+//   + group3: {
+//         fee: 100500
+//     }
+// }`;
 
 test('test JSON', () => {
   expect(gendiff(beforeJson, afterJson)).toBe(equal);
@@ -27,4 +82,8 @@ test('test YAML', () => {
 
 test('test INI', () => {
   expect(gendiff(beforeIni, afterIni)).toBe(equal);
+});
+
+test('test nestedJSON', () => {
+  expect(gendiff(nestedBeforeJson, nestedAfterJson)).toBe(nestedEqual);
 });
--- src/index.js
+++ src/index.js
@@ -7,29 +7,138 @@ const fileToString = file => fs.readFileSync(file, 'utf8');
 
 const getExtension = file => path.extname(file).slice(1);
 
+
+const differenceObjects = (firstObject, secondObject) => {
+  const firstKeys = Object.keys(firstObject);
+  const secondKeys = Object.keys(secondObject);
+  const uniqKeys = _.union(firstKeys, secondKeys);
+
+  const arrayDiff = uniqKeys.reduce((acc, key) => {
+    if (key in firstObject && key in secondObject) {
+      if (firstObject[key] instanceof Object || secondObject[key] instanceof Object) {
+        const result = { type: 'noChangedChildren',
+          body: {
+            key,
+            value: differenceObjects(firstObject[key], secondObject[key]),
+          },
+        };
+        return [...acc, result];
+      }
+
+      if (firstObject[key] === secondObject[key]) {
+        const result = { type: 'noChanged',
+          body: {
+            key,
+            value: firstObject[key],
+          },
+        };
+        return [...acc, result];
+      }
+
+      const result = {
+        type: 'changed',
+        body: {
+          key,
+          valueNew: secondObject[key],
+          valueOld: firstObject[key],
+        },
+      };
+      return [...acc, result];
+    }
+
+    if (!(key in secondObject)) {
+      // if (firstObject[key] instanceof Object) {
+      //   const result = {
+      //     type: 'deletedChildren',
+      //     body: {
+      //       key,
+      //       value: toString(firstObject[key]),
+      //     },
+      //   };
+      //   console.log(firstObject[key]);
+      //   return [...acc, result];
+      // }
+      const result = {
+        type: 'deleted',
+        body: {
+          key,
+          value: firstObject[key],
+        },
+      };
+      return [...acc, result];
+    }
+    // if (secondObject[key] instanceof Object) {
+    //   const result = {
+    //     type: 'addChildren',
+    //     body: {
+    //       key,
+    //       value: toString(secondObject[key]),
+    //     },
+    //   };
+    //   return [...acc, result];
+    // }
+    const result = {
+      type: 'add',
+      body: {
+        key,
+        value: secondObject[key],
+      },
+    };
+    return [...acc, result];
+  }, []);
+  return arrayDiff;
+};
+
+const getString = (ast) => {
+  // console.log(ast);
+
+  const result = ast.reduce((acc, obj) => {
+    // console.log(key.body.value);
+    // if (key.body.value.toString() === 'object') {
+    //   console.log(`.toString()`);
+    //   return '';
+    // }
+    if (obj.type === 'noChanged') {
+      return `${acc}\n  ${obj.body.key}: ${obj.body.value}`;
+    }
+    if (obj.type === 'changed') {
+      return `${acc}\n+ ${obj.body.key}: ${obj.body.valueNew}\n- ${obj.body.key}: ${obj.body.valueOld}`;
+    }
+    if (obj.type === 'deleted') {
+      // if (obj.body.value instanceof Object) {
+      //   console.log(obj.body.value);
+      //   return `${acc}\n+ ${obj.body.key}:
+      //     ${Object.keys(obj.body.value)[0]} ${obj.body.value[0]}`;
+      // }
+      return `${acc}\n- ${obj.body.key}: ${obj.body.value}`;
+    }
+    if (obj.type === 'add') {
+      return `${acc}\n+ ${obj.body.key}: ${obj.body.value}`;
+    }
+    if (obj.type === 'noChangedChildren') {
+      return `${acc}\n  ${obj.body.key}: ${getString(obj.body.value)}`;
+    }
+    if (obj.type === 'deletedChildren') {
+      return `${acc}\n- ${obj.body.key}: ${getString(obj.body.value)}`;
+    }
+    if (obj.type === 'addChildren') {
+      return `${acc}\n+ ${obj.body.key}: ${getString(obj.body.value)}`;
+    }
+    return `${acc}`;
+  }, '');
+  return `{${result}\n}`;
+};
+
 const gendiff = (firstConfigFile, secondConfigFile) => {
   const firstString = fileToString(firstConfigFile);
   const secondString = fileToString(secondConfigFile);
   const extension = getExtension(firstConfigFile);
   const firstObj = getParse(extension)(firstString);
   const secondObj = getParse(extension)(secondString);
-  const firstKeys = Object.keys(firstObj);
-  const secondKeys = Object.keys(secondObj);
-  const uniqKeys = _.union(firstKeys, secondKeys);
 
-  const arrayDiff = uniqKeys.reduce((acc, key) => {
-    if (key in firstObj && key in secondObj) {
-      if (firstObj[key] === secondObj[key]) {
-        return acc.concat(` ${key}: ${firstObj[key]}`);
-      }
-      return acc.concat(`+ ${key}: ${secondObj[key]}\n- ${key}: ${firstObj[key]}`);
-    }
-    if (!(key in secondObj)) {
-      return acc.concat(`- ${key}: ${firstObj[key]}`);
-    }
-    return acc.concat(`+ ${key}: ${secondObj[key]}`);
-  }, []);
-  return `{\n ${arrayDiff.join('\n')}\n}`;
+  const ast = differenceObjects(firstObj, secondObj);
+  const result = getString(ast);
+  return result;
 };
 
 export default gendiff;
