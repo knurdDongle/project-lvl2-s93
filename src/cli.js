5674c854662665254154a78847aff277018a406a
krivtsov <sergey@krivtsov.su>
2017-07-14 01:01:05 +0400

    fix cli

--- src/cli.js
+++ src/cli.js
@@ -3,11 +3,11 @@ import gendiff from '.';
 
 export default () => {
   program
-   .version('1.2.1')
-   .arguments('<firstConfig> <secondConfig>')
-   .description('Compares two configuration files and shows a difference.')
-   .option('-f, --format [type]', 'Output format')
-   .action((first, second) => console.log(gendiff(first, second, program.format)));
+    .version('1.2.1')
+    .arguments('<firstConfig> <secondConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .action((first, second) => console.log(gendiff(first, second, program.format)));
 
   program.parse(process.argv);
 };
